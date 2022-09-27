1bd9070f618c4ed80aa5812de0636daf3340c87a
Hettore Eduardo <duh.barretoo@gmail.com>
2022-09-27 18:38:52 -0300

    Condição ternária 1.

--- src/entities/ResultadoEscolar.java
+++ src/entities/ResultadoEscolar.java
@@ -4,18 +4,11 @@ public class ResultadoEscolar {
 
 	public static void main(String[] args) {
 
-		int nota = 6;
+		int nota = 9;
 
-		if (nota >= 7) {
-			System.out.println("Aprovado!");
-		} 
-		else if (nota >= 5 && nota <7) {
-			System.out.println("Prova de Recuperação!");
-		
-		} else {
-			System.out.println("Reprovado!");
-		}
+		String resultado = (nota >= 7) ? "Aprovado!" : "Reprovado!";
 
+		System.out.println(resultado);
 	}
 
 }
